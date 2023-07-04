6743fd94866afa7fc1b1297b95b975bfe69c6899
AhmedElslt2020 <114757305+AhmedElslt2020@users.noreply.github.com>
2023-07-04 16:58:36 +0300

    Add files via upload

--- task_school_eng/models/student_student.py
+++ task_school_eng/models/student_student.py
@@ -11,6 +11,31 @@ class StudentStudent(models.Model):
     class_id = fields.Many2one("class.class")
     class_year = fields.Many2one(related="class_id.year_id")
     subject_ids = fields.One2many("subject.subject","student_id")
+    date_of_birth = fields.Date(string='Date of Birth')
+    number_of_student = fields.Char(string='Number of Student')
+    age = fields.Integer(string='Age', compute='_compute_age', store=True)
+    gender = fields.Selection(string="Gender", selection=[('male', 'Male'), ('female', 'Female')], default='male')
+
+    @api.depends('date_of_birth')
+    def _compute_age(self):
+        for rec in self:
+            today = date.today()
+            if rec.date_of_birth:
+                rec.age = today.year - rec.date_of_birth.year
+            else:
+                rec.age = 1
+
+    @api.constrains("age")
+    def check_something(self):
+        for record in self:
+            if record.age < 7:
+                raise ValidationError(_("the age is smaller than seven"))
+
+    _sql_constraints = [
+        ('unique_number_of_student',
+         'UNIQUE(number_of_student)',
+         "The number is not unique you must change it"),
+    ]
 
 
 
